59f75d2d88a492c1c2df6df7b104132b8600f305
Thomas Dacey <DaceyTom2@gmail.com>
2021-02-25 11:08:52 -0500

    Mocha tests

---
+++ test/test.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+
+describe('ArrayPass', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe('ArrayFail', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(3), -1);
+    });
+  });
+});
