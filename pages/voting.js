b7776f41df3a3da583f910be22919a86da139124
Thomas Dacey <DaceyTom2@gmail.com>
2021-02-26 15:24:27 -0500

    implemented cards

---
+++ pages/voting.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import CardContainer from "../components/card/cardContainer";
+
+export async function getStaticProps() {
+  const allPostsData = {votingCards:[
+    {key:"Hulk", name:"The Hulk",description:"Strong green man",image:"/images/hulk.png"},
+    {key:"Spiderman", name:"Spiderman",description:"Spider like senses",image:"/images/spiderman.png"},
+    {key:"Ironman", name:"Ironman",description:"Tech enabled nerd",image:"/images/ironman.png"}
+  ]};
+  
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Voting({ allPostsData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <h1>Voting tiles</h1>
+        <p>Select a tile you'd like to place your vote on</p>
+      </section>
+      <section>
+        <CardContainer votingCards={ allPostsData.votingCards } />
+      </section>
+    </Layout>
+  );
+}
