da15bf71266fed3db6feeeeef5ced29e8385abff
Thomas Dacey <DaceyTom2@gmail.com>
2021-03-01 10:41:33 -0500

    linted votes file

--- pages/api/votes.js
+++ pages/api/votes.js
@@ -1,8 +1,7 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
     let votableId = req.body.votableId;
-    if (votableId)
-      addVote(votableId);
+    if (votableId) addVote(votableId);
     res.status(200).json({ text: "Post" });
   } else if (req.method === "GET") {
     res.status(200).json({ votables: votables, totalVotes: totalVotes });
@@ -10,19 +9,19 @@ export default function handler(req, res) {
 }
 
 let totalVotes = 0;
-let votables = []
-addVotable("tom")
-addVotable("rick")
+let votables = [];
+addVotable("tom");
+addVotable("rick");
 
 export function addVote(votableId) {
   let validateVote = true;
-  if (validateVote){
-    votables.find(x => x.id === votableId).votes++
+  if (validateVote) {
+    votables.find((x) => x.id === votableId).votes++;
     totalVotes++;
   }
 }
 
-export function addVotable(name){
-  let votable = { id: name, name: name, votes: 0 }
-  votables.push(votable)
+export function addVotable(name) {
+  let votable = { id: name, name: name, votes: 0 };
+  votables.push(votable);
 }
